28f5d6283305733731e265a80e8fe604584d77bf
Emilia Penney <elijahpenney@outlook.com>
2022-05-18 04:25:29 -0700

    Added JSDoc and cleaned up return values

--- src/components/Ticker/helpers/getNextTick.ts
+++ src/components/Ticker/helpers/getNextTick.ts
@@ -1,7 +1,12 @@
-export const getNextTick = (baseDuration: number) => {
-    const now = new Date();
-    const seconds = now.getSeconds() + 1;
-    const flooredSeconds = Math.ceil(seconds / baseDuration) * baseDuration;
-    now.setSeconds(flooredSeconds);
-    return now;
+/**
+ * Calculates the time for the next tick event
+ * @param duration The number in seconds till the next tick event
+ * @returns The DateTime of the next tick event
+ */
+export const getNextTick = (duration: number) => {
+  const now = new Date();
+  const seconds = now.getSeconds() + 1;
+  const flooredSeconds = Math.ceil(seconds / duration) * duration;
+  now.setSeconds(flooredSeconds);
+  return now;
 };
--- src/components/Ticker/helpers/getPercentage.ts
+++ src/components/Ticker/helpers/getPercentage.ts
@@ -1,3 +1,9 @@
+/**
+ * Calculates the percentage of time completed towards the next tick event
+ * @param nextTick The DateTime of the next tick event
+ * @param duration The duration used to calculate the offset till the next tick event
+ * @returns A percentage of completion till the next tick event
+ */
 export const getPercentage = (nextTick: Date, duration: number) => {
   if (!nextTick) {
     throw new Error('Attempted to get percentage but nextTick is not set');
@@ -10,9 +16,14 @@ export const getPercentage = (nextTick: Date, duration: number) => {
 
   const output = parseInt((diff * 100).toFixed(0));
 
+  /**
+   * This is used to compensate for any drift the JS engine has
+   * as during testing it was giving negative percentages
+   * due to the system processing faster or slow than expected
+   */
   if (output < 0) {
     return 0;
   }
 
-  return parseInt((diff * 100).toFixed(0));
+  return output;
 };
