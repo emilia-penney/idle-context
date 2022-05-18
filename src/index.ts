b501df441389eadaae97e30f4802e661d4254a00
Emilia Penney <elijahpenney@outlook.com>
2022-05-18 04:40:44 -0700

    Added exports

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { TickerOutput } from './types';
+import { CurrencyContext, CurrencyContextProvider, Ticker } from './components';
+
+export {
+  TickerOutput,
+  Ticker,
+  CurrencyContext,
+  CurrencyContextProvider,
+};
