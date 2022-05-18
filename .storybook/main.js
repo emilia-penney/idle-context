6577d59d8b022595585ca57181e2208d45987cb4
Emilia Penney <elijahpenney@outlook.com>
2022-05-18 04:07:00 -0700

    Added storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react"
-}
+  addons: ['@storybook/addon-essentials'],
+  babel: async (options) => ({
+    // Update your babel configuration here
+    ...options,
+  }),
+  framework: '@storybook/react',
+  stories: ['../src/**/*.stories.@(js|mdx|tsx)'],
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need
+    // Return the altered config
+    return config;
+  },
+};
