57e5443bfb9e4d51432bec4addd722a1d9dbd06b
Emilia Penney <elijahpenney@outlook.com>
2022-05-21 16:40:47 -0700

    Broke out files into logical components

--- src/components/CurrencyContext/CurrencyContext.tsx
+++ src/components/CurrencyContext/CurrencyContext.tsx
@@ -1,57 +1,7 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { CurrencyChange, CurrencyContextI, CurrencyContextProps } from '../../types';
+import { createContext } from 'react';
+import { CurrencyContextI } from '../../types';
 
 export const CurrencyContext = createContext<CurrencyContextI>({
   currencies: {},
   updateCurrency: () => true,
 });
-
-export const CurrencyContextProvider: React.FC<CurrencyContextProps> = ({ children, currencies: propsCurrencies }) => {
-  const [currencies, setCurrencies] = useState(propsCurrencies);
-  const [pendingChanges, setPendingChanges] = useState<CurrencyChange[]>([]);
-
-
-  useEffect(() => {
-    if (!pendingChanges.length) {
-      return;
-    }
-
-    const operation = pendingChanges.shift();
-
-    if (operation) {
-      const newCurrencies = { ...currencies };
-      newCurrencies[operation.name] = operation.amount + newCurrencies[operation.name];
-      setCurrencies(newCurrencies);
-    }
-
-    }, [pendingChanges.length]);
-
-  /**
-   * Modifies the value of an existing currency
-   * @param name The name of the currency to be updated
-   * @param amount A positive or negative number to modify the currency by
-   */
-  const updateCurrency: CurrencyContextI['updateCurrency'] = async (name, amount) => {
-    if (currencies?.[name] === undefined) {
-      throw new Error(`Attempted to update ${name} currency but currency does not exist`);
-    }
-
-    if (amount === 0) {
-      return;
-    }
-
-    const newPendingChanges = [...pendingChanges, { name, amount }];
-
-    setPendingChanges(newPendingChanges);
-  };
-
-
-  return (
-    <CurrencyContext.Provider value={{
-      currencies,
-      updateCurrency,
-    }}>
-      {children}
-    </CurrencyContext.Provider>
-  );
-};
