49b18e5dc95776a0d905a84e7792df156d8c2e09
Emilia Penney <elijahpenney@outlook.com>
2022-05-21 14:43:43 -0700

    Added calculated duration and docs

--- src/components/Ticker/Ticker.tsx
+++ src/components/Ticker/Ticker.tsx
@@ -4,26 +4,37 @@ import { TickerProps, TickerOutput } from '../../types/';
 import { CurrencyContext } from '../CurrencyContext';
 import { getNextTick, getPercentage } from './helpers';
 
-export const Ticker: React.FC<TickerProps> = ({ children, duration, valuePerTick, currency }) => {
+/**
+ * A component wrapper that handles ticker logic
+ * @param args.children A visual component that accepts TickerOutput props
+ * @param args.duration The original length of time the ticker should run for
+ * @param args.valuePerTick The amount of value / money produced per tick
+ * @param args.currency The type of currency to increase based on CurrencyContext
+ * @param args.speedCalculation A function that takes in a the level and outputs a new duration
+ * @param args.level The level of the ticker
+ * @returns A visual component wrapped with a ticker logic wrapper
+ */
+export const Ticker: React.FC<TickerProps> = ({ children, duration, valuePerTick, currency, speedCalculation, level }) => {
   const id = uuid();
   const [nextTick, setNextTick] = useState<Date>(getNextTick(duration));
   const [initalizing, setInitializing] = useState<boolean>(true);
   const [percentage, setPercentage] = useState<number>(0);
   const [loop, setLoop] = useState<NodeJS.Timer>();
+  const [calcDuration, setCalcDuration] = useState<number>(duration);
 
   const { updateCurrency } = useContext(CurrencyContext);
 
   const onTick = () => {
     return new Promise((resolve) => {
       updateCurrency(currency, valuePerTick);
-      setNextTick(getNextTick(duration));
+      setNextTick(getNextTick(calcDuration));
       resolve(true);
     });
   };
 
   useEffect(() => {
     const run = () => {
-      setNextTick(getNextTick(duration));
+      setNextTick(getNextTick(calcDuration));
       setInitializing(false);
     };
 
@@ -62,12 +73,21 @@ export const Ticker: React.FC<TickerProps> = ({ children, duration, valuePerTick
 
     };
 
-    runLoop(nextTick, duration);
+    runLoop(nextTick, calcDuration);
 
     return () => {
       clearTimeout(loop);
     };
   }, [nextTick, initalizing]);
+
+  useEffect(() => {
+    if (!speedCalculation) {
+      setCalcDuration(calcDuration * 0.9);
+      return;
+    }
+
+    setCalcDuration(speedCalculation(level));
+  }, [level]);
 
   if (initalizing) {
     return (<></>);
