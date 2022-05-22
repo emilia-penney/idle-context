6f32b2edf445322f82c563d51808d74330ff6b69
Emilia Penney <elijahpenney@outlook.com>
2022-05-21 20:44:21 -0700

    Moved storybook to typescript

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,12 @@
 module.exports = {
   addons: ['@storybook/addon-essentials'],
-  babel: async (options) => ({
+  babel: async (options: any) => ({
     // Update your babel configuration here
     ...options,
   }),
   framework: '@storybook/react',
   stories: ['../src/**/*.stories.@(js|mdx|tsx)'],
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config: any, { configType }: any) => {
     // Make whatever fine-grained changes you need
     // Return the altered config
     return config;
